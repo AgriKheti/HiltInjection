9744ee34b5f66fff8caed5ad2423cbe09ddc4762
agrikheti <gurwinder.agrikheti@gmail.com>
2022-05-12 13:42:08 +0530

    lat code 1:42pm

--- app/src/main/java/com/agrikheti/hiltinjection/ui/fragments/userdetails/UserDetails.kt
+++ app/src/main/java/com/agrikheti/hiltinjection/ui/fragments/userdetails/UserDetails.kt
@@ -9,10 +9,7 @@ import com.agrikheti.hiltinjection.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-/**
- * A simple [Fragment] subclass.
- * Use the [UserDetails.newInstance] factory method to
- * create an instance of this fragment.
+/*
  */
 class UserDetails : Fragment() {
     // TODO: Rename and change types of parameters
