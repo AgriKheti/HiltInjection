a9a3f4b90c32eb5210ca2e65d149f36c7e82a76d
agrikheti <gurwinder.agrikheti@gmail.com>
2022-05-12 13:43:48 +0530

    lat code

--- app/src/main/java/com/agrikheti/hiltinjection/ui/fragments/userdetails/UserDetails.kt
+++ app/src/main/java/com/agrikheti/hiltinjection/ui/fragments/userdetails/UserDetails.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import com.agrikheti.hiltinjection.R
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 /*
  */
 class UserDetails : Fragment() {
